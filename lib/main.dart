f7dcb1997575989636d7942bc910d6e04f20d67e
Navaneeth B <navaneeth0041@gmail.com>
2025-11-10 18:23:22 +0530

    feat: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hopin/presentation/routes/route_names.dart';
+import 'firebase_options.dart';
 import 'data/providers/user_profile_provider.dart';
 import 'presentation/routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.light,
     ),
   );
-  
+
   runApp(const MyApp());
 }
 
